d8cdced875c1068db9b8463fd052b060f5b41af1
CatrinPP <caterina.parshina@gmail.com>
2020-06-11 00:34:27 +0300

    базовая стилизация

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("src/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("src/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "src/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("src/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("src/*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("css", "server"));
